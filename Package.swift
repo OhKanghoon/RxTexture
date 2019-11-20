787ec4ec57c80d65f70a7c08e313d57a53483bc6
OhKanghoon <ggaa96@naver.com>
2019-11-21 01:54:20 +0900

    Add rxexpect dependency

--- Package.swift
+++ Package.swift
@@ -5,16 +5,17 @@ import PackageDescription
 let package = Package(
     name: "RxTexture",
     platforms: [
-      .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
+      .iOS(.v9)
     ],
     products: [
         .library(name: "RxTexture", targets: ["RxTexture"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/devxoul/RxExpect.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
         .target(name: "RxTexture", dependencies: ["RxSwift", "RxCocoa"]),
-        .testTarget(name: "RxTextureTests", dependencies: ["RxTexture"]),
+        .testTarget(name: "RxTextureTests", dependencies: ["RxTexture", "RxExpect"]),
     ]
 )
