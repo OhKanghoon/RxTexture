383c1ae252000df8f171cc3c2fdbb8b66fb7edfe
OhKanghoon <ggaa96@naver.com>
2020-01-12 21:32:40 +0900

    public to extension

--- Sources/RxTexture/ASImageNode+Rx.swift
+++ Sources/RxTexture/ASImageNode+Rx.swift
@@ -9,8 +9,8 @@ import AsyncDisplayKit
 import RxCocoa
 import RxSwift
 
-extension Reactive where Base: ASImageNode {
-  public var image: Binder<UIImage?> {
+public extension Reactive where Base: ASImageNode {
+  var image: Binder<UIImage?> {
     return Binder(self.base, scheduler: CurrentThreadScheduler.instance) { node, image in
       node.image = image
     }
