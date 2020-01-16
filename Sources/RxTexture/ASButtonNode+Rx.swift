409e9d89e4bce3e3a9cd0fab3398cc2dbc711544
OhKanghoon <ggaa96@naver.com>
2020-01-17 01:37:19 +0900

    Add ASButtonNode+Rx

---
+++ Sources/RxTexture/ASButtonNode+Rx.swift
@@ -0,0 +1,34 @@
+//
+//  ASButtonNode+Rx.swift
+//  RxTexture
+//
+//  Created by KanghoonOh on 17/01/2020.
+//
+
+import AsyncDisplayKit
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: ASButtonNode {
+
+  /// Reactive wrapper for `setAttributedTitle(_:for:)`
+  func attributedTitle(for controlState: UIControl.State = []) -> Binder<NSAttributedString?> {
+    return Binder(self.base, scheduler: CurrentThreadScheduler.instance) { node, attributedTitle in
+      node.setAttributedTitle(attributedTitle, for: controlState)
+    }
+  }
+
+  /// Reactive wrapper for `setImage(_:for:)`
+  func image(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+    return Binder(self.base, scheduler: CurrentThreadScheduler.instance) { node, image -> Void in
+      node.setImage(image, for: controlState)
+    }
+  }
+
+  /// Reactive wrapper for `setBackgroundImage(_:for:)`
+  func backgroundImage(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+    return Binder(self.base, scheduler: CurrentThreadScheduler.instance) { node, image -> Void in
+      node.setBackgroundImage(image, for: controlState)
+    }
+  }
+}
