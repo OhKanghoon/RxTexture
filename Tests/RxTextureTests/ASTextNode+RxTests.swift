3bb950b0e297ef4f85fc3490091db13865c6af01
OhKanghoon <ggaa96@naver.com>
2020-03-02 18:13:13 +0900

    fix indent

--- Tests/RxTextureTests/ASTextNode+RxTests.swift
+++ Tests/RxTextureTests/ASTextNode+RxTests.swift
@@ -13,11 +13,11 @@ import RxTexture
 
 final class ASTextNodeTests: XCTestCase {
   func testASTextNode_attributedTextObserver() {
-      let node = ASTextNode()
-      XCTAssertEqual(node.attributedText, nil)
-      let text = NSAttributedString(string: "Hello!")
-      _ = Observable.just(text).bind(to: node.rx.attributedText)
-
-      XCTAssertEqual(node.attributedText, text)
+    let node = ASTextNode()
+    XCTAssertEqual(node.attributedText, nil)
+    let text = NSAttributedString(string: "Hello!")
+    _ = Observable.just(text).bind(to: node.rx.attributedText)
+    
+    XCTAssertEqual(node.attributedText, text)
   }
 }
